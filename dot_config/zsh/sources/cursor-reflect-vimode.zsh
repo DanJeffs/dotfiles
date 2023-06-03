87f4dec72d056403486a0bea81e91841d1ca0b8b
DanJeffs <danieljeffs83@gmail.com>
2023-06-03 19:48:25 +1000

    Added Files

---
+++ dot_config/zsh/sources/cursor-reflect-vimode.zsh
@@ -0,0 +1,58 @@
+#!/bin/zsh
+
+
+# ########################### --ViMode Cursor Shape-- ####################### #
+
+
+# ----------- Cursor-Change with Mode-Change -----------
+# Change cursor shape for different vi modes.
+# 'echo -ne' : don't print newline at end and read '\'
+function zle-keymap-select {
+    if
+        [[ ${KEYMAP} == vicmd ]] ||
+        [[ $1 = 'block' ]]
+    then
+        echo -ne '\e[1 q'
+    elif
+        [[ ${KEYMAP} == main ]] ||
+        [[ ${KEYMAP} == viins ]] ||
+        [[ ${KEYMAP} = '' ]] ||
+        [[ $1 = 'beam' ]]
+    then
+        echo -ne '\e[5 q'
+    fi
+}
+zle -N zle-keymap-select
+# ^create user-defined widget ('zle -N <func>')
+
+
+# ----------- Cursor+Mode Change with NewLine -----------
+# this script starts us in <insert> mode AND sets cursor to blink-beam
+# and fires on every new line
+zle-line-init() {
+    zle -K viins
+    # ^select keymap ('zle -K <selected_keymap>'); starts us in <insert> mode
+    echo -ne "\e[5 q"
+}
+zle -N zle-line-init
+# ^create user-defined widget ('zle -N <func>')
+
+
+# ----------- Cursor-Change with Terminal -----------
+# Use beam shape cursor on startup
+echo -ne '\e[5 q'
+
+
+########### back-pocket function ###########
+# INFO: as far as I can tell this behavior is reundant, 
+#       but weaker, than zle-line-init()'s
+#       Commenting out, but leaving for possible future use.
+#
+# # ----------- Cursor-Change before Command-Processing -----------
+# # 'preexec()': executed right before entered command is
+# #  so it should effectively set the cursor shape for the next line
+# # (useful when a command is entered in <normal> mode,
+# #  to prevent inheriting the block-cursor shape)
+# #  NOTE: does NOT fire if no command (neither "good" nor "bad") is entered
+# #        so '[enter]' on a blank line in <normal> mode will not fire preexec()
+# preexec() { echo -ne '\e[5 q' ;}
